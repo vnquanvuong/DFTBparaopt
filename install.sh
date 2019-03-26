be86ce51eb5b3e2f8cda2f87f46e72edd1f6c5d7
Van Quan Vuong <v2quan89@gmail.com>
2019-03-26 12:59:43 -0400

    change install.sh for MAC OS

--- install.sh
+++ install.sh
@@ -21,54 +21,54 @@ else
   echo "the compiler $CXX is not supported. Please select g++ or icpc"
   exit
 fi
-#cd galib247
-#make clean
-#make install | tee make.log
-#cd ../
-#
-#if [ ! -f 3.3.7.tar.gz ]; then
-#  wget http://bitbucket.org/eigen/eigen/get/3.3.7.tar.gz
-#fi
-#rm -rf eigen-eigen-323c052e1731
-#tar -xf 3.3.7.tar.gz
-#rm -rf eigen3.37
-#mv eigen-eigen-323c052e1731 eigen3.37
-#
-#cd repopt
-#if [ $CXX == "g++" ]; then
-#  cp makefile.gnu makefile
-#elif [ $CXX == "icpc" ]; then
-#  cp makefile.intel makefile
-#else
-#  echo "the compiler $CXX is not supported. Please select g++ or icpc"
-#  exit
-#fi
-#make clean 
-#make all | tee make.log
-#cd ../
-#
-#cd erepopt
-#if [ $CXX == "g++" ]; then
-#  cp makefile.gnu makefile
-#elif [ $CXX == "icpc" ]; then
-#  cp makefile.intel makefile
-#else
-#  echo "the compiler $CXX is not supported. Please select g++ or icpc"
-#  exit
-#fi
-#make clean 
-#make all | tee make.log
-#cd ../
-#
-#cd utils 
-#if [ $CXX == "g++" ]; then
-#  cp makefile.gnu makefile
-#elif [ $CXX == "icpc" ]; then
-#  cp makefile.intel makefile
-#else
-#  echo "the compiler $CXX is not supported. Please select g++ or icpc"
-#  exit
-#fi
-#make clean 
-#make all | tee make.log
-#cd ../
+cd galib247
+make clean
+make install | tee make.log
+cd ../
+
+if [ ! -f 3.3.7.tar.gz ]; then
+  wget http://bitbucket.org/eigen/eigen/get/3.3.7.tar.gz
+fi
+rm -rf eigen-eigen-323c052e1731
+tar -xf 3.3.7.tar.gz
+rm -rf eigen3.37
+mv eigen-eigen-323c052e1731 eigen3.37
+
+cd repopt
+if [ $CXX == "g++" ]; then
+  cp makefile.gnu makefile
+elif [ $CXX == "icpc" ]; then
+  cp makefile.intel makefile
+else
+  echo "the compiler $CXX is not supported. Please select g++ or icpc"
+  exit
+fi
+make clean 
+make all | tee make.log
+cd ../
+
+cd erepopt
+if [ $CXX == "g++" ]; then
+  cp makefile.gnu makefile
+elif [ $CXX == "icpc" ]; then
+  cp makefile.intel makefile
+else
+  echo "the compiler $CXX is not supported. Please select g++ or icpc"
+  exit
+fi
+make clean 
+make all | tee make.log
+cd ../
+
+cd utils 
+if [ $CXX == "g++" ]; then
+  cp makefile.gnu makefile
+elif [ $CXX == "icpc" ]; then
+  cp makefile.intel makefile
+else
+  echo "the compiler $CXX is not supported. Please select g++ or icpc"
+  exit
+fi
+make clean 
+make all | tee make.log
+cd ../
